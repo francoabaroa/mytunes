eb00d8a24121966acbad5fae93cafb71c10e6c05
francoabaroa <franco9395@gmail.com>
2016-09-29 18:25:41 -0700

    Added last test, bare min requirements complete

--- test/spec/SongQueueViewSpec.js
+++ test/spec/SongQueueViewSpec.js
@@ -23,6 +23,12 @@ describe('SongQueueView', function() {
     expect(SongQueueEntryView.prototype.render).to.have.been.called;
   });
 
+  it('should have a header element after being rendered', function() {
+    view = new SongQueueView({ collection: fakeSongs });
+    expect(view.$el.children().length).to.equal(2);
+    expect(view.$el.children()[0].tagName).to.equal('TH');
+  });
+
   it('renders when add or remove event fires from the song queue collection', function() {
     sinon.spy(SongQueueView.prototype, 'render');
     view = new SongQueueView({collection: fakeSongs});
